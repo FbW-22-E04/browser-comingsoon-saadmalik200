8f80d91b12e0fc3f973a5eb06952c6639ed035bb
Saad Malik <saad_malik200@hotmail.com>
2022-09-28 09:00:03 +0200

    ex1 changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import './styles/main.scss'; // Don't delete :)
-import smile from './scripts/smile.js'; // Feel free to delete :)
+import moment from "moment";
+import "./styles/main.scss"; // Don't delete :)
+const countDown = require("moment-countdown");
 
 // All of your javascript should go here :)
 
+const newYear = new Date("1 January 2023");
+console.log(newYear);
 
-smile() // Feel free to delete :)
+const now = Date.now();
+console.log(now);
+
+const days = 1000 * 60 * 60 * 24;
+const hours = 1000 * 60 * 60;
+const minutes = 1000 * 60;
+const seconds = 1000;
+
+const diff = newYear - now;
+const daysLeft = Math.floor(diff / days);
+const hoursLeft = Math.floor((diff % days) / hours);
+const minutesLeft = Math.floor(((diff / days) % hours) / minutes / seconds);
+
+console.log(daysLeft, hoursLeft, minutesLeft);
+// console.log(days);
+// console.log(hours);
